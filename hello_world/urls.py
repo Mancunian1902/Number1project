b73c93aea4dc92edbf07c23896d9a499ded22a79
Mancunian1902 <victor_rogovsky@hotmail.com>
2020-08-03 02:29:20 +0600

    HTML some layout created

--- hello_world/urls.py
+++ hello_world/urls.py
@@ -4,7 +4,7 @@ from hello_world import views
 urlpatterns = [
     path('hello_world/', views.hello_world, name='hello_world'),
     #path('', views.about_company, name='about_company'), was fixed by VL
-    path('about/', views.about_company, name='about_company'),
+    path('about_company/', views.about_company, name='about_company'),
     path('news/', views.news, name='news'),
     path('contact_us/', views.contact_us, name='contact_us'),
     path('our_team/', views.our_team, name='our_team'),
